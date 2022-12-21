6d43db288029ea845a4eb59a6715f3c1cb5ced41
USER-NB <pacmanbear@mail.ru>
2022-12-21 10:31:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/materialdesign/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.materialdesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+        ArrayList<String> posts = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            posts.add("Post " + i);
+            Log.i("Posts: ", posts.get(i));
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, posts);
+        listView.setAdapter(arrayAdapter);
+    }
+}
